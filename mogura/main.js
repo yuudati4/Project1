91aa1bd57efdc84f81a1ae0778295aa183572ef3
yuudati4 <k22126kk@aitech.ac.jp>
2023-06-13 15:32:53 +0900

    mogura

--- mogura/main.js
+++ mogura/main.js
@@ -14,23 +14,29 @@ var assets = [
     'images/wack_a_mole_hole_2_b.png',
     'images/wack_a_mole_hole_3_a.png',
     'images/wack_a_mole_hole_3_b.png',
-    
+
     // もぐら
     'images/wack_a_mole_mole.png',
     'images/wack_a_mole_mole_hit_1.png',
     //エフェクト
     'images/wack_a_mole_effect_ouch.png'
- 
+
 ];
 
-function gameStart(){// ゲーム画面
+
+
+function gameStart() {// ゲーム画面
     var scene = new Scene();
     core.replaceScene(scene); core.resume();
-   
+
+
+
 
     //==========
     // ここから
     //==========
+
+
 
     //空
     scene.backgroundColor = "skyblue";
@@ -54,44 +60,64 @@ function gameStart(){// ゲーム画面
 
 
 
+
+
     //モグラ生成のfunction
-    function mole_set(x, y, j){
-        
+    function mole_set(x, y, j) {
+
         var hole = new Sprite(82, 28);
-        if(j==0){
+        if (j == 0) {
             hole.image = core.assets['images/wack_a_mole_hole_1_a.png'];
-        }else if(j==1){
+        } else if (j == 1) {
             hole.image = core.assets['images/wack_a_mole_hole_2_a.png'];
-        }else{
+        } else {
             hole.image = core.assets['images/wack_a_mole_hole_3_a.png'];
         }
-        
-        hole.x = x-16;
+
+        hole.x = x - 16;
         hole.y = y;
         scene.addChild(hole);
 
-        var mole = new Sprite(50,62);
-        mole.image = core.assets['images/wack_a_mole_mole.png'];
-        mole.x = x;
-        mole.y = y;
-        scene.addChild(mole);
-        function moveMole(mole){
-            var wait = getRandom(5, 200);
-            var speed = getRandom(5, 20);
+
+
+
+
+
+
+
+        function moveMole(mole) {
+            var wait = getRandom(40, 200);
+            var speed = getRandom(8, 20);
             mole.tl.delay(wait);
             mole.tl.moveBy(0, -45, speed);
             mole.tl.delay(speed);
             mole.tl.moveBy(0, 45, speed);
-            mole.tl.then(function() {
+            mole.tl.then(function () {
+                if (mole.y <= y) {
+                    mole.image = core.assets['images/wack_a_mole_mole.png']
+                }
                 moveMole(mole);
             });
         }
-        moveMole(mole);
-        
-        var n = 0;
-        mole.addEventListener(enchant.Event.TOUCH_START, function(e){            
 
-            if(n==0){
+
+
+        var mole = new Sprite(50, 62);
+        mole.image = core.assets['images/wack_a_mole_mole.png'];
+        mole.x = x;
+        mole.y = y;
+        scene.addChild(mole);
+        moveMole(mole);
+
+
+
+        var n = 0;
+
+
+
+
+        mole.addEventListener(enchant.Event.TOUCH_START, function (e) {
+            if (n == 0) {
                 var ouch = new Sprite(47, 22);
                 ouch.image = core.assets['images/wack_a_mole_effect_ouch.png'];
                 ouch.x = this.x;
@@ -100,77 +126,117 @@ function gameStart(){// ゲーム画面
                 scene.addChild(ouch);
                 ouch.tl.scaleTo(1, 1, 3);
                 ouch.tl.and();
-                ouch.tl.moveTo(this.x+15, this.y-10, 3);
+                ouch.tl.moveTo(this.x + 15, this.y - 10, 3);
                 ouch.tl.delay(10);
-                ouch.tl.then(function(){
-                ouch.remove();
-            });
-                scoreLabel.score += 1;
-            }
-            n += 1;
-            this.image = core.assets['images/wack_a_mole_mole_hit_1.png'];
-            this.tl.clear();
-            this.tl.moveTo(x, y, 32);
-            this.tl.then(function(){
-                if(this.y<=y){
+                ouch.tl.then(function () {
+                    ouch.remove();
+                });
+
+
+
+                this.image = core.assets['images/wack_a_mole_mole_hit_1.png'];
+                this.tl.clear();
+                this.tl.moveTo(x, y, 32);
+                this.tl.then(function () {
                     this.image = core.assets['images/wack_a_mole_mole.png'];
                     this.y = y;
                     n = 0;
-                } 
+                });
 
-            });
-            
-            
+
+
+                n += 1;
+                scoreLabel.score += 1;
+                moveMole(mole);
+
+
+
+            } else if (n > 0) {
+                this.image = core.assets['images/wack_a_mole_mole_hit_1.png'];
+                this.tl.clear();
+                this.tl.moveTo(x, y, 32);
+                this.tl.then(function () {
+                    this.image = core.assets['images/wack_a_mole_mole.png'];
+                    this.y = y;
+                    n = 0;
+
+
+
+                });
+                moveMole(mole);
+
+
+
+            }
+
+
+
+
         });
-        
+
+
+
+
+
         var hole2 = new Sprite(82, 28);
-        if(j==0){
+        if (j == 0) {
             hole2.image = core.assets['images/wack_a_mole_hole_1_b.png'];
-        }else if(j==1){
+        } else if (j == 1) {
             hole2.image = core.assets['images/wack_a_mole_hole_2_b.png'];
-        }else{
+        } else {
             hole2.image = core.assets['images/wack_a_mole_hole_3_b.png'];
         }
-        hole2.x = mole.x-16;
+        hole2.x = mole.x - 16;
         hole2.y = mole.y;
         scene.addChild(hole2);
 
+
+
         var background1 = new Sprite(60, 70);
-        if(j==0){
+        if (j == 0) {
             background1.image = core.assets['images/wack_a_mole_grass_bg_1_b.png'];
-        }else if(j==1){
+        } else if (j == 1) {
             background1.image = core.assets['images/wack_a_mole_grass_bg_2_b.png'];
-        }else{
+        } else {
             background1.image = core.assets['images/wack_a_mole_grass_bg_3_b.png'];
         }
-        background1.x = x-10;
-        background1.y = y+21;
+        background1.x = x - 10;
+        background1.y = y + 21;
         scene.addChild(background1);
     }
 
-    for(var i=0;i<3;i++){
-        for(var j=0;j<3;j++){
-            var x = 35+(i*100);
-            var y = 130*j+130;
+
+
+    //モグラの表示
+
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var x = 35 + (i * 100);
+            var y = 130 * j + 130;
             mole_set(x, y, j);
         }
     }
+
+
+
 
 
     //==========
     // ここまで
     //==========
 
+
+
     // タイム
     timeLabel = new Label('時間 : 30秒');
-    timeLabel.x = core.width+36;
+    timeLabel.x = core.width + 36;
     timeLabel.y = 30;
     timeLabel.color = 'black';
     timeLabel.scale(3, 3);
     timeLabel.score = 30;
     scene.addChild(timeLabel);
     scene.tl.setTimeBased();
-    scene.tl.delay(1000).then(function() {
+    scene.tl.delay(1000).then(function () {
         timeLabel.score -= 1;
         timeLabel.text = '時間 : ' + timeLabel.score + '秒';
         if (timeLabel.score == 0) {
@@ -187,24 +253,35 @@ function gameStart(){// ゲーム画面
         }
     }).loop();
 
+
+
     // スコア
-    scoreLabel = new Label('得点 : 0点');
-    scoreLabel.x = core.width+36;
+    scoreLabel = new Label();
+    scoreLabel.x = core.width + 36;
     scoreLabel.y = 520;
     scoreLabel.scale(3, 3);
     scoreLabel.color = 'black';
     scoreLabel.score = 0;
     scene.addChild(scoreLabel);
-    scoreLabel.addEventListener(Event.ENTER_FRAME, function() {
+
+    scoreLabel.addEventListener(Event.ENTER_FRAME, function () {
         scoreLabel.text = '得点 : ' + scoreLabel.score + '点';
     });
+
+
+
+
 }
+
+
 
 function getRandom(start, end) {
-    return start + Math.floor( Math.random() * (end - start + 1));
+    return start + Math.floor(Math.random() * (end - start + 1));
 }
 
-function titleStart(){// タイトル画面
+
+
+function titleStart() {// タイトル画面
     var scene = new Scene();
     scene.backgroundColor = 'black';
     core.replaceScene(scene); core.pause();
@@ -215,17 +292,19 @@ function titleStart(){// タイトル画面
     label.y = core.height / 2 - label._boundHeight / 2;
     label.color = 'white';
     scene.addChild(label);
-    scene.on(enchant.Event.TOUCH_START, function(){gameStart();});
+    scene.on(enchant.Event.TOUCH_START, function () { gameStart(); });
 }
+
+
 
 //==========
 // EnchantJS
 var core;
 enchant();
-window.onload = function() {
-core = new Core(320, 600);
-core.fps = 16;
-core.preload(assets);
-core.onload = function(){titleStart();};
-core.start();
+window.onload = function () {
+    core = new Core(320, 600);
+    core.fps = 16;
+    core.preload(assets);
+    core.onload = function () { titleStart(); };
+    core.start();
 };
